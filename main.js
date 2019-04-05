88dee7097862bc6a0096ed58cc36faf3b000602a
Justin Yau <justiceman8998@gmail.com>
2019-04-05 13:46:18 -0400

    Bot is now fully functional with a working bot token.

--- main.js
+++ main.js
@@ -8,6 +8,7 @@
  */
 
 var Discord = require('discord.js'); // npm install discord.js
+var unirest = require('unirest');    // npm install unirest
 var logger = require('winston');     // npm install winston
 /**
    auth.json format:
@@ -51,6 +52,30 @@ bot.on('message', message => {
         case 'prefix':
             prefix = args[0];
             message.channel.send("Prefix successfully changed to: " + prefix);
+            break;
+        case 'server':
+            var server_id = args[0];
+            unirest.get("https://api.battlemetrics.com/servers/".concat(server_id))
+                .end(function (result) {
+                    //console.log(result.status, result.headers, result.body);
+                    var json = JSON.parse(JSON.stringify(result.body));
+                    if(result.status != 200) {
+                        message.reply("An error occurred while trying to make the API request!");
+                    } else {
+                        message.channel.send("**Server Information**:\n" +
+                                                "\tGame: " + json.data.relationships.game.data.id + "\n" +
+                                                "\tServer id: " + json.data.id + "\n" +
+                                                "\tServer name: " + json.data.attributes.name + "\n" +
+                                                "\tServer IP: " + json.data.attributes.ip + "\n" +
+                                                "\tOfficial: " + json.data.attributes.details.official + "\n" +
+                                                "\tMap: " + json.data.attributes.details.map + "\n" +
+                                                "\tPlayers: " + json.data.attributes.players + "\n" +
+                                                "\tMax Players: " + json.data.attributes.maxPlayers + "\n" +
+                                                "\tServer Rank: " + json.data.attributes.rank + "\n" +
+                                                "\tPVE Enabled: " + json.data.attributes.details.pve + "\n" +
+                                                "\tStatus: " + json.data.attributes.status + "\n");
+                    }
+                });
             break;
         default:
             message.channel.send("Command: " + command + " not recognized!");
