444b750b13db3b34b8019bb9da44f7c05af029c1
Justin Yau <justiceman8998@gmail.com>
2019-04-05 12:38:49 -0400

    Added basic implementation of the bot. Needs to make API requests and format the results to send back to the user.

---
+++ main.js
@@ -0,0 +1,37 @@
+var Discord = require('discord.js');
+var logger = require('winston');
+var auth = require('./auth.json');
+
+// Configure logger settings
+logger.remove(logger.transports.Console);
+logger.add(new logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+
+// Initialize Discord Bot
+var bot = new Discord.Client();
+
+bot.once('ready', function (evt) {
+    logger.info('Connected!');
+});
+
+bot.login(auth.token);
+
+bot.on('message', message => {
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.content.substring(0, 1) == '!') {
+        var args = message.content.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                message.channel.send("Pong!");
+                break;
+            // Just add any case commands if you want to..
+        }
+    }
+});
